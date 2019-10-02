ba47e767f84849d043bfcbf4148996c044779e01
Adam <a.zartin@mail.utoronto.ca>
2019-10-02 01:11:50 -0400

    change error alert back

--- src/pages/Investment/Investment.js
+++ src/pages/Investment/Investment.js
@@ -127,6 +127,7 @@ export default class Investment extends Component {
         .catch((err)=>{
             //triggers a state change which will refresh all components
             this.showAlert(err.response.data.code,'error');
+            console.log(err)
         });
 
     }
